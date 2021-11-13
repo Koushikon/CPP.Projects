2b9f7e7b986db5e0a9cdab3c7d8e82e56ae7c83b
Koushikon <54022602+Koushikon@users.noreply.github.com>
2021-11-13 23:02:21 +0530

    add raylib 4.0 support and configure setup

--- _raylib-Template/main.cpp
+++ _raylib-Template/main.cpp
@@ -1,9 +1,31 @@
-#include <iostream>
-
 #include "raylib.h"
 
 int main(void)
 {
+    const int screenWidth{800};
+    const int screenHeight{600};
+
+    InitWindow(screenWidth, screenHeight, "✔ basic window");
+
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+
+    while (!WindowShouldClose()) // Detect window close button or ESC key
+    {
+        BeginDrawing();
+
+        ClearBackground(WHITE);
+
+        /**
+         * 
+         * Code Here... 
+         * 
+         */
+
+
+        EndDrawing();
+    }
+
+    CloseWindow(); // Close window and OpenGL context
 
     return 0;
 }
